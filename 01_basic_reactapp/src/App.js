6704e14afbe177f126a56953ccdbfb5a00abca7b
MD NADEEM SARWAR <mdnadeem.mn289@gmail.com>
2023-08-24 16:29:38 +0530

    first react app using basic and vite tool

---
+++ 01_basic_reactapp/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React | Nadeem Sarwar</h1>
+  );
+}
+
+export default App;
---
+++ 01_vite_reactapp/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur React with Vite | Nadeem</h1>
+  )
+}
+
+export default App
