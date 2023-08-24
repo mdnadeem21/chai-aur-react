07011cf17f2405f467c9a4f69edd74938680f732
MD NADEEM SARWAR <mdnadeem.mn289@gmail.com>
2023-08-24 20:10:20 +0530

    learn about file structure and code flow

--- 01_basic_reactapp/src/App.js
+++ 01_basic_reactapp/src/App.js
@@ -1,7 +1,10 @@
-
+import Chai from "./chai";
 function App() {
   return (
+    <>
     <h1>Chai aur React | Nadeem Sarwar</h1>
+    <Chai/>
+    </>
   );
 }
 
--- 01_vite_reactapp/src/App.jsx
+++ 01_vite_reactapp/src/App.jsx
@@ -1,11 +1,13 @@
-
+import Chai from './chai'
 
 function App() {
   
 
   return (
-    <h1>Chai aur React with Vite | Nadeem</h1>
-  )
+    <>
+    <Chai/>
+    </>
+    )
 }
 
 export default App
